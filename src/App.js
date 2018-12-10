8bf8bfc7ac7f6f9a9319ee2b13220339b098efdc
holyeternal <habbo_armageddon@hotmail.com>
2018-12-10 18:12:09 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './components/data';
+import { Button, Icon, Table, Row, Col, Collapsible, CollapsibleItem, Footer } from 'react-materialize';
 
 class App extends Component {
+  
   render() {
+    const listItems = data.map((x) =>
+    <CollapsibleItem key={x.toString()}
+              header={x} />
+
+  );
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div class="navbar-fixed">
+          <nav>
+            <div class="nav-wrapper">
+              <a href="#!" class="brand-logo">Logo</a>
+              <ul class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <br />
+        <div className="container">
+          <Row>
+            <Col s={4} className='grid-example'>
+              <Collapsible>
+                <CollapsibleItem header={listItems} icon='filter_drama'>
+                  Maths
+  </CollapsibleItem>
+              </Collapsible>
+            </Col>
+            <Col s={8} className='grid-example'>
+              <Table>
+                <thead>
+                  <tr>
+                    
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr>
+                    <td>Alvin</td>
+                    <td>Eclair</td>
+                    <td>$0.87</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </div>
+        <Footer copyrights="&copy 2015 Copyright Text"
+          moreLinks={
+            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+          }
+          links={
+            <ul>
+              <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+            </ul>
+          }
+          className='example'
+        >
+          <h5 className="white-text">Footer Content</h5>
+          <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+        </Footer>
       </div>
     );
   }
+  
 }
 
 export default App;
---
+++ src/components/data.js
@@ -0,0 +1,18 @@
+const initialData = {
+    task: {
+      "task-1": { id: "task-1", content: "Math" },
+      "task-2": { id: "task-2", content: "Science" },
+      "task-3": { id: "task-3", content: "English" },
+      "task-4": { id: "task-4", content: "Chinese" }
+    },
+    column: {
+      TOOLBOX: {
+        id: "TOOLBOX",
+        title: "TOOLBOX",
+        taskIds: ["task-1", "task-2", "task-3", "task-4"]
+      }
+       
+    }
+  };
+  
+  export default initialData;
